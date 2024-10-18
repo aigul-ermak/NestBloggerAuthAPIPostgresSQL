428c16ef37a66a92969f7f8d192f5199d97e45a5
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-17 21:19:27 -0300

    Added files for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './settings/app.controller';
+import { AppService } from './settings/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
