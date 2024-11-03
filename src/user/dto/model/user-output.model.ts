f3f69a214a09a85f66d445167833efaba2e7690d
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-03 13:48:02 -0400

    Added UserOutputModelMapper: for get all users functionality

--- src/user/dto/model/user-output.model.ts
+++ src/user/dto/model/user-output.model.ts
@@ -1,9 +1,22 @@
+import { User } from '../../entities/user.entity';
+
 export class UserOutputModel {
   id: number;
   login: string;
   email: string;
   createdAt: Date;
 }
+
+export const UserOutputModelMapper = (user: User): UserOutputModel => {
+  const outputModel = new UserOutputModel();
+
+  outputModel.id = user.id;
+  outputModel.login = user.login;
+  outputModel.email = user.email;
+  outputModel.createdAt = user.createdAt;
+
+  return outputModel;
+};
 
 export class UserWithIdOutputModel {
   id: string;
