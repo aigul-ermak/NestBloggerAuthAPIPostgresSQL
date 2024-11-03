cb6ba5482db6eaea7eb0ace8af2ccd67aff1daa1
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-03 13:47:06 -0400

    Added find all method

--- src/user/repositories/users-query.repository.ts
+++ src/user/repositories/users-query.repository.ts
@@ -1,6 +1,6 @@
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectDataSource } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
-import { FindOneOptions, Repository } from 'typeorm';
+import { DataSource, FindOneOptions } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -30,5 +30,46 @@ export class UsersQueryRepository {
     const result = await this.dataSource.query(query, parameters);
 
     return result.length > 0 ? result[0] : null;
+  }
+
+  async findAll(
+    filter: any,
+    sortBy: string,
+    sortDirection: 'asc' | 'desc',
+    skip: number,
+    limit: number,
+  ): Promise<User[]> {
+    const query = `
+      SELECT * FROM "user"
+      ${this.buildWhereClause(filter)}
+      ORDER BY "${sortBy}" ${sortDirection.toUpperCase()}
+      LIMIT $1 OFFSET $2
+    `;
+    const parameters = [limit, skip];
+    const result = await this.dataSource.query(query, parameters);
+
+    return result;
+  }
+
+  async countDocuments(filter: any): Promise<number> {
+    const query = `
+      SELECT COUNT(*) FROM "user"
+      ${this.buildWhereClause(filter)}
+    `;
+
+    const result = await this.dataSource.query(query);
+    return parseInt(result[0].count, 10);
+  }
+
+  private buildWhereClause(filter: any): string {
+    if (!filter || !filter.$or || filter.$or.length === 0) return '';
+
+    const whereConditions = filter.$or.map((condition: any) => {
+      const [field, regexCondition] = Object.entries(condition)[0];
+      const regexValue = (regexCondition as any).$regex;
+      return `"${field}" ILIKE '%${regexValue}%'`;
+    });
+
+    return `WHERE ${whereConditions.join(' OR ')}`;
   }
 }
