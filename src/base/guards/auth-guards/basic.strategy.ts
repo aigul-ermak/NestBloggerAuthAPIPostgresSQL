7511d9df7c009b860ee93028850fea3f7d536b90
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-03 12:49:28 -0400

    Added basic strategy and guard

---
+++ src/base/guards/auth-guards/basic.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { BasicStrategy as Strategy } from 'passport-http';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class BasicStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      passReqToCallback: true,
+    });
+  }
+
+  public validate = async (req, username, password): Promise<boolean> => {
+    const basicAuthUsername = this.configService.get<string>(
+      'basicAuthSettings.BASIC_AUTH_USERNAME',
+    );
+    const basicAuthPassword = this.configService.get<string>(
+      'basicAuthSettings.BASIC_AUTH_PASSWORD',
+    );
+
+    if (basicAuthUsername === username && basicAuthPassword === password) {
+      return true;
+    }
+
+    throw new UnauthorizedException();
+  };
+}
