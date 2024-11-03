f88ca9d0ea9d0ee64b41d1aef5e50dfb4d66db59
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-03 13:49:39 -0400

    Updated User controller: added findAll end point

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateUserUseCaseCommand } from './usecases/createUserUseCase';
+import { BasicAuthGuard } from '../base/guards/auth-guards/basic.auth.guard';
+import { UserOutputModel } from './dto/model/user-output.model';
+import { GetAllUsersUseCaseCommand } from './usecases/getAllUsersUseCase';
+import { SortUserDto } from './dto/sort-user.dto';
 
-@Controller('sa')
+@Controller('sa/users')
 export class UserController {
   constructor(private commandBus: CommandBus) {}
 
-  @Post('users')
-  create(@Body() createUserDto: CreateUserDto) {
+  @Post()
+  @HttpCode(201)
+  @UseGuards(BasicAuthGuard)
+  create(@Body() createUserDto: CreateUserDto): Promise<UserOutputModel> {
     return this.commandBus.execute(new CreateUserUseCaseCommand(createUserDto));
   }
 
-  // @Get()
-  // findAll() {
-  //   return this.userService.findAll();
-  // }
+  @Get()
+  @HttpCode(201)
+  @UseGuards(BasicAuthGuard)
+  findAll(@Query() sortData: SortUserDto) {
+    return this.commandBus.execute(new GetAllUsersUseCaseCommand(sortData));
+  }
   //
   // @Get(':id')
   // findOne(@Param('id') id: string) {
