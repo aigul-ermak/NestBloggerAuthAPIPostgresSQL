588c01d6eeeede016560339125e3ee656ec3e477
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-03 13:47:36 -0400

    Added dataSourse: but commented for now

--- src/user/repositories/users.repository.ts
+++ src/user/repositories/users.repository.ts
@@ -8,11 +8,15 @@ export class UsersRepository {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    // @InjectDataSource()
+    // private dataSource: DataSource,
   ) {}
 
   async create(user: Partial<User>): Promise<User> {
     const newUser = this.usersRepository.create(user);
     return await this.usersRepository.save(newUser);
+    // todo get raw sql only, post/put typeorm
+    // return await this.dataSource.query(`INSERT INTO "user" VALUES ()`);
   }
 
   async save(user: Partial<User>): Promise<User> {
