77a82a255d135f952839bddd42aec13752fb8791
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-03 13:46:42 -0400

    Added dataSourse and refactored findOne user method

--- src/user/repositories/users-query.repository.ts
+++ src/user/repositories/users-query.repository.ts
@@ -6,11 +6,29 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersQueryRepository {
   constructor(
-    @InjectRepository(User)
-    private readonly usersRepository: Repository<User>,
+    // @InjectRepository(User)
+    // private readonly usersRepository: Repository<User>,
+    @InjectDataSource()
+    private dataSource: DataSource,
   ) {}
 
   async findOne(criteria: FindOneOptions<User>): Promise<User | null> {
-    return await this.usersRepository.findOne(criteria);
+    const whereClauses = [];
+    const parameters = [];
+    if (criteria.where) {
+      Object.entries(criteria.where).forEach(([key, value], index) => {
+        whereClauses.push(`"${key}" = $${index + 1}`);
+        parameters.push(value);
+      });
+    }
+
+    const query = `
+      SELECT * FROM "user" 
+      ${whereClauses.length ? 'WHERE ' + whereClauses.join(' AND ') : ''}
+      LIMIT 1
+    `;
+    const result = await this.dataSource.query(query, parameters);
+
+    return result.length > 0 ? result[0] : null;
   }
 }
