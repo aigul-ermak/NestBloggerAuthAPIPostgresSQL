694026d2b656e8a5b2b5de31abb6c9be8814b2e3
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-05 10:46:40 -0400

    Updated users module: remove Post entity

--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -6,13 +6,12 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { CreateUserUseCase } from './usecases/createUserUseCase';
 import { UsersRepository } from './repositories/users.repository';
 import { UsersQueryRepository } from './repositories/users-query.repository';
-import { Post } from '../post/entities/post.entity';
 import { GetAllUsersUseCase } from './usecases/getAllUsersUseCase';
 
 const CommandHandlers = [CreateUserUseCase, GetAllUsersUseCase];
 
 @Module({
-  imports: [CqrsModule, TypeOrmModule.forFeature([User, Post])],
+  imports: [CqrsModule, TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [...CommandHandlers, UsersRepository, UsersQueryRepository],
   exports: [UsersRepository, UsersQueryRepository],
