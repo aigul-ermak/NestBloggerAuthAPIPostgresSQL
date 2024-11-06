6bcad42f55db9dedcc7a796c8bfdd6913049c788
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-06 14:34:08 -0400

    Updated main: wrapped app with applyAppSettings

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import { ConfigService } from '@nestjs/config';
 import { ConfigurationType } from './settings/configuration';
+import { applyAppSettings } from '@app/settings/apply.app.setting';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  applyAppSettings(app);
 
   const configService =
     app.get<ConfigService<ConfigurationType>>(ConfigService);
