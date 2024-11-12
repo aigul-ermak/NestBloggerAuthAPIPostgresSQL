167e3420dbecd242497e95dc13d17a0aeb774aa7
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-12 15:20:47 -0400

    Added email adapter service

---
+++ src/features/email/email-adapter.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import nodemailer from 'nodemailer';
+
+@Injectable()
+export class EmailAdapter {
+  private transporter;
+
+  constructor(private configService: ConfigService) {
+    const user = this.configService.get<string>('emailSettings.EMAIL_USER');
+    const pass = this.configService.get<string>('emailSettings.EMAIL_PASS');
+
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: user,
+        pass: pass,
+      },
+    });
+  }
+
+  async sendEmail(to: string, subject: string, message: string): Promise<void> {
+    try {
+      await this.transporter.sendMail({
+        from: this.configService.get<string>('emailSettings.EMAIL_USER'),
+        to,
+        subject,
+        html: message,
+      });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Failed to send email');
+    }
+  }
+}
