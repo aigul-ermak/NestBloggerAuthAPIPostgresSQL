42f38906f09e160c929e439d39969b218d9d765d
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-14 10:26:26 -0400

    Updated Auth controller: added session module

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,13 +1,48 @@
-import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { CreateUserRegistrationUseCaseCommand } from './usecases/createUserRegistrationUseCase';
+import { Request, Response } from 'express';
+import { UserLoginDto } from './models/login-user.input.dto';
+import { LoginUserUseCaseCommand } from './usecases/loginUserUseCase';
 
 @UseGuards(ThrottlerGuard)
 @Controller('auth')
 export class AuthController {
   constructor(private commandBus: CommandBus) {}
+
+  @Post('/login')
+  @HttpCode(200)
+  async login(
+    @Body() loginDto: UserLoginDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const userIP: string = req.ip ?? 'testuserip';
+    const userAgent: string = req.headers['user-agent'] ?? 'user-agent';
+
+    const { accessToken, refreshToken } = await this.commandBus.execute(
+      new LoginUserUseCaseCommand(loginDto, userIP, userAgent),
+    );
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ accessToken });
+  }
 
   @Post('/registration')
   @HttpCode(204)
--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -6,11 +6,34 @@ import { UserModule } from '../user/user.module';
 import { BasicStrategy } from '../../base/guards/auth-guards/basic.strategy';
 import { CreateUserRegistrationUseCase } from './usecases/createUserRegistrationUseCase';
 import { EmailModule } from '../email/email.module';
+import { LoginUserUseCase } from './usecases/loginUserUseCase';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SessionModule } from '../session/session.module';
 
-const CommandHandlers = [CreateUserRegistrationUseCase];
+const CommandHandlers = [CreateUserRegistrationUseCase, LoginUserUseCase];
 
 @Module({
-  imports: [CqrsModule, UserModule, EmailModule],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('jwtSettings.JWT_ACCESS_SECRET'),
+          signOptions: {
+            expiresIn: configService.get<string>(
+              'jwtSettings.ACCESS_TOKEN_EXPIRY',
+            ),
+          },
+        };
+      },
+    }),
+    CqrsModule,
+    UserModule,
+    EmailModule,
+    SessionModule,
+  ],
   providers: [...CommandHandlers, AuthService, BasicStrategy],
   controllers: [AuthController],
 })
