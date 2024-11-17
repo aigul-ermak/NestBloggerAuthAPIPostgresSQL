5ed12a8b9499c2ffe09ac006d90c7ddb79c5e156
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-11-17 10:46:39 -0400

    Updated userController: getAll users functionality

--- src/features/user/user.controller.ts
+++ src/features/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateUserUseCaseCommand } from './usecases/createUserUseCase';
 import { BasicAuthGuard } from '../../base/guards/auth-guards/basic.auth.guard';
 import { UserOutputModel } from './dto/model/user-output.model';
+import { SortUserDto } from './dto/sort-user.dto';
+
+import { GetAllUsersUseCaseCommand } from './usecases/getAllUsersUseCase';
 
 @Controller('sa/users')
 export class UserController {
@@ -16,12 +27,12 @@ export class UserController {
     return this.commandBus.execute(new CreateUserUseCaseCommand(createUserDto));
   }
 
-  // @Get()
-  // @HttpCode(200)
-  // @UseGuards(BasicAuthGuard)
-  // findAll(@Query() sortData: SortUserDto) {
-  //   return this.commandBus.execute(new GetAllUsersUseCaseCommand(sortData));
-  // }
+  @Get()
+  @HttpCode(200)
+  @UseGuards(BasicAuthGuard)
+  findAll(@Query() sortData: SortUserDto) {
+    return this.commandBus.execute(new GetAllUsersUseCaseCommand(sortData));
+  }
 
   //
   // @Get(':id')
