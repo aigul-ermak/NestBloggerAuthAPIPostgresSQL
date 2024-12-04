aa7e83c87923f250a52a26ac34559c9ca19fd475
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-12-04 13:45:22 -0400

    Change in tests

--- test/user/auth.e2e-spec.ts
+++ test/user/auth.e2e-spec.ts
@@ -42,93 +42,93 @@ describe('Auth testing', () => {
     await app.close();
   });
 
-  it('POST -> "/auth/registration": should return 204: user registered', async () => {
-    const userRegistrationDto = {
-      login: 'user',
-      password: 'password',
-      email: 'example@example.com',
-    };
-
-    const registrationUser = await request(httpServer)
-      .post(`/auth/registration`)
-      .set(
-        'Authorization',
-        getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS),
-      )
-      .send(userRegistrationDto)
-      .expect(204);
-  });
-
-  it('POST -> "/auth/registration": return 400 for user registration', async () => {
-    const userRegistrationDto = {
-      login: '',
-      password: '',
-      email: '',
-    };
-
-    const response = await request(httpServer)
-      .post('/auth/registration')
-      .set(
-        'Authorization',
-        getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS),
-      )
-      .send(userRegistrationDto)
-      .expect(400);
-
-    const expectedResult = {
-      errorsMessages: [
-        {
-          message: 'Length not correct',
-          field: 'login',
-        },
-        {
-          message: 'Length not correct',
-          field: 'password',
-        },
-        {
-          message: 'email must be an email',
-          field: 'email',
-        },
-      ],
-    };
-
-    expect(response.body).toEqual(expectedResult);
-  });
-
-  it('POST -> auth/login: should return 200 for login user', async () => {
-    const newUserDto = {
-      login: 'user1',
-      password: 'password',
-      email: 'example1@example.com',
-    };
-
-    const newUserResponse = await createUser(
-      app,
-      newUserDto,
-      HTTP_BASIC_USER,
-      HTTP_BASIC_PASS,
-    );
-
-    expect(newUserResponse.status).toBe(201);
-
-    const response = await request(httpServer)
-      .post(`/auth/login`)
-      .set(
-        'Authorization',
-        getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS),
-      )
-      .send({
-        loginOrEmail: newUserDto.login,
-        password: newUserDto.password,
-      })
-      .expect(200);
-
-    const accessToken = response.body;
-
-    const expectedResult = {
-      accessToken: expect.any(String),
-    };
-
-    expect(response.body).toEqual(expectedResult);
-  });
+  // it('POST -> "/auth/registration": should return 204: user registered', async () => {
+  //   const userRegistrationDto = {
+  //     login: 'user',
+  //     password: 'password',
+  //     email: 'example@example.com',
+  //   };
+  //
+  //   const registrationUser = await request(httpServer)
+  //     .post(`/auth/registration`)
+  //     .set(
+  //       'Authorization',
+  //       getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS),
+  //     )
+  //     .send(userRegistrationDto)
+  //     .expect(204);
+  // });
+  //
+  // it('POST -> "/auth/registration": return 400 for user registration', async () => {
+  //   const userRegistrationDto = {
+  //     login: '',
+  //     password: '',
+  //     email: '',
+  //   };
+  //
+  //   const response = await request(httpServer)
+  //     .post('/auth/registration')
+  //     .set(
+  //       'Authorization',
+  //       getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS),
+  //     )
+  //     .send(userRegistrationDto)
+  //     .expect(400);
+  //
+  //   const expectedResult = {
+  //     errorsMessages: [
+  //       {
+  //         message: 'Length not correct',
+  //         field: 'login',
+  //       },
+  //       {
+  //         message: 'Length not correct',
+  //         field: 'password',
+  //       },
+  //       {
+  //         message: 'email must be an email',
+  //         field: 'email',
+  //       },
+  //     ],
+  //   };
+  //
+  //   expect(response.body).toEqual(expectedResult);
+  // });
+  //
+  // it('POST -> auth/login: should return 200 for login user', async () => {
+  //   const newUserDto = {
+  //     login: 'user1',
+  //     password: 'password',
+  //     email: 'example1@example.com',
+  //   };
+  //
+  //   const newUserResponse = await createUser(
+  //     app,
+  //     newUserDto,
+  //     HTTP_BASIC_USER,
+  //     HTTP_BASIC_PASS,
+  //   );
+  //
+  //   expect(newUserResponse.status).toBe(201);
+  //
+  //   const response = await request(httpServer)
+  //     .post(`/auth/login`)
+  //     .set(
+  //       'Authorization',
+  //       getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS),
+  //     )
+  //     .send({
+  //       loginOrEmail: newUserDto.login,
+  //       password: newUserDto.password,
+  //     })
+  //     .expect(200);
+  //
+  //   const accessToken = response.body;
+  //
+  //   const expectedResult = {
+  //     accessToken: expect.any(String),
+  //   };
+  //
+  //   expect(response.body).toEqual(expectedResult);
+  // });
 });
