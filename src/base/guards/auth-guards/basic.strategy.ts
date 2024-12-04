5a34b7f48fe9040e1687cbafbfa6b0ebbd730cc6
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-12-04 14:11:10 -0400

    Change in tests

--- src/base/guards/auth-guards/basic.strategy.ts
+++ src/base/guards/auth-guards/basic.strategy.ts
@@ -2,27 +2,46 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { BasicStrategy as Strategy } from 'passport-http';
 import { ConfigService } from '@nestjs/config';
+//
+// @Injectable()
+// export class BasicStrategy extends PassportStrategy(Strategy) {
+//   constructor(private configService: ConfigService) {
+//     super({
+//       passReqToCallback: true,
+//     });
+//   }
+//
+//   public validate = async (req, username, password): Promise<boolean> => {
+//     const basicAuthUsername = this.configService.get<string>(
+//       'basicAuthSettings.BASIC_AUTH_USERNAME',
+//     );
+//     const basicAuthPassword = this.configService.get<string>(
+//       'basicAuthSettings.BASIC_AUTH_PASSWORD',
+//     );
+//
+//     if (basicAuthUsername === username && basicAuthPassword === password) {
+//       return true;
+//     }
+//
+//     throw new UnauthorizedException();
+//   };
+// }
 
 @Injectable()
 export class BasicStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
-    super({
-      passReqToCallback: true,
-    });
+    super();
   }
 
-  public validate = async (req, username, password): Promise<boolean> => {
-    const basicAuthUsername = this.configService.get<string>(
-      'basicAuthSettings.BASIC_AUTH_USERNAME',
-    );
-    const basicAuthPassword = this.configService.get<string>(
-      'basicAuthSettings.BASIC_AUTH_PASSWORD',
-    );
+  async validate(username: string, password: string): Promise<boolean> {
+    const basicAuthSettings = this.configService.get('basicAuthSettings');
 
-    if (basicAuthUsername === username && basicAuthPassword === password) {
+    if (
+      username === basicAuthSettings.BASIC_AUTH_USERNAME &&
+      password === basicAuthSettings.BASIC_AUTH_PASSWORD
+    ) {
       return true;
     }
-
     throw new UnauthorizedException();
-  };
+  }
 }
