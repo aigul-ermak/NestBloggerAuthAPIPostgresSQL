2bdbb258a396df431815e05c6becc2de49e0abe2
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-16 12:41:55 -0400

    Modified docker-compose file and initial scripts

--- sql/init-scripts/init_blogs.sql
+++ sql/init-scripts/init_blogs.sql
@@ -1,8 +1,20 @@
--- Create the production database
-CREATE DATABASE blogs;
+-- Check if the 'blogs' database exists, and create it if not
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'blogs') THEN
+        CREATE DATABASE blogs;
+    END IF;
+END
+$$;
 
 -- Create a user for the production database
-CREATE USER admin WITH ENCRYPTED PASSWORD 'kumon';
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+        CREATE USER admin WITH ENCRYPTED PASSWORD 'kumon';
+    END IF;
+END
+$$;
 
 -- Grant privileges to the user
 GRANT ALL PRIVILEGES ON DATABASE blogs TO admin;
--- sql/init-scripts/init_test.sql
+++ sql/init-scripts/init_test.sql
@@ -1,8 +1,20 @@
--- Create the test database
-CREATE DATABASE test_blogs;
+-- Check if the 'test_blogs' database exists, and create it if not
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'test_blogs') THEN
+        CREATE DATABASE test_blogs;
+    END IF;
+END
+$$;
 
--- Create a user for the test database
-CREATE USER admin WITH ENCRYPTED PASSWORD '54321';
+-- Create a user for the test database if it doesn't already exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+        CREATE USER admin WITH ENCRYPTED PASSWORD '54321';
+    END IF;
+END
+$$;
 
 -- Grant privileges to the user
 GRANT ALL PRIVILEGES ON DATABASE test_blogs TO admin;
