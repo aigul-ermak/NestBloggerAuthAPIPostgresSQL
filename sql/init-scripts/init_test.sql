9197488776e51e502b7262d78d71b05305487af9
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-17 18:50:33 -0400

    Commented initial scripts, modifies docker-compose.yaml

--- sql/init-scripts/init_test.sql
+++ sql/init-scripts/init_test.sql
@@ -1,20 +1,12 @@
--- Create the 'test_blogs' database if it doesn't exist
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'test_blogs') THEN
-        RAISE NOTICE 'Creating database test_blogs';
-    END IF;
-END
-$$;
-
--- Create a user for the test database if it doesn't already exist
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
-        CREATE USER admin WITH ENCRYPTED PASSWORD '54321';
-    END IF;
-END
-$$;
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON DATABASE test_blogs TO admin;
+--
+-- -- Create a user for the test database if it doesn't already exist
+-- DO $$
+-- BEGIN
+--     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+--         CREATE USER admin WITH ENCRYPTED PASSWORD '54321';
+--     END IF;
+-- END
+-- $$;
+--
+-- -- Grant privileges to the user
+-- GRANT ALL PRIVILEGES ON DATABASE test_blogs TO admin;
