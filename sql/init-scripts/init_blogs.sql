a6d49211fdd4a7049c5a362290c0c7dfcc47c308
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-17 18:50:30 -0400

    Commented initial scripts, modifies docker-compose.yaml

--- sql/init-scripts/init_blogs.sql
+++ sql/init-scripts/init_blogs.sql
@@ -1,11 +1,11 @@
--- Create a user for the production database
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
-        CREATE USER admin WITH ENCRYPTED PASSWORD 'kumon';
-    END IF;
-END
-$$;
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON DATABASE blogs TO admin;
+-- -- Create a user for the production database
+-- DO $$
+-- BEGIN
+--     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+--         CREATE USER admin WITH ENCRYPTED PASSWORD 'kumon';
+--     END IF;
+-- END
+-- $$;
+--
+-- -- Grant privileges to the user
+-- GRANT ALL PRIVILEGES ON DATABASE blogs TO admin;
