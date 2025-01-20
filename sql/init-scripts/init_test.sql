0873cb76decc01679937eb283cf9d0a23dde4f4b
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-20 11:00:57 -0400

    Uncommented initial scripts,

--- sql/init-scripts/init_test.sql
+++ sql/init-scripts/init_test.sql
@@ -1,12 +1,12 @@
 --
--- -- Create a user for the test database if it doesn't already exist
--- DO $$
--- BEGIN
---     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
---         CREATE USER admin WITH ENCRYPTED PASSWORD '54321';
---     END IF;
--- END
--- $$;
---
--- -- Grant privileges to the user
--- GRANT ALL PRIVILEGES ON DATABASE test_blogs TO admin;
+-- Create a user for the test database if it doesn't already exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+        CREATE USER admin WITH ENCRYPTED PASSWORD '54321';
+    END IF;
+END
+$$;
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON DATABASE test_blogs TO admin;
