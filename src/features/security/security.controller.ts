5ca381b54d9a1c7478483769c21d99c8e218f82f
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-28 18:41:06 -0400

    Updated Security endpoints functionality

--- src/features/security/security.controller.ts
+++ src/features/security/security.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { SecurityService } from './security.service';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { CommandBus } from '@nestjs/cqrs';
+import { RefreshTokenGuard } from '../../base/guards/jwt-guards/refresh-token.guard';
+import { GetAllDevicesWithActiveSessionsUseCaseCommand } from './usecases/getAllDevicesWithActiveSessionsUseCase';
 
 @Controller('security')
 export class SecurityController {
-  constructor(private readonly securityService: SecurityService) {}
+  constructor(private commandBus: CommandBus) {}
+
+  @Get('/devices')
+  @HttpCode(200)
+  @UseGuards(RefreshTokenGuard)
+  async getAllDevicesWithActiveSessions(@Req() request: Request) {
+    console.log('inside endpoint');
+
+    if (!request.user)
+      throw new UnauthorizedException('User info was not provided');
+
+    const { userId } = request.user;
+
+    const activeSessions = await this.commandBus.execute(
+      new GetAllDevicesWithActiveSessionsUseCaseCommand(userId),
+    );
+
+    return activeSessions;
+  }
 }
