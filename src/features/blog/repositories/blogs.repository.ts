091b61227f9a95dc7badfcf531ab6b58701470ae
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-04 12:40:30 -0400

    Updated BlogsRepository: added createBlog method

--- src/features/blog/repositories/blogs.repository.ts
+++ src/features/blog/repositories/blogs.repository.ts
@@ -4,4 +4,18 @@ import { Pool } from 'pg';
 @Injectable()
 export class BlogsRepository {
   constructor(@Inject('DATABASE_POOL') private readonly pool: Pool) {}
+
+  async createBlog(name, description, websiteUrl) {
+    const query = `
+      INSERT INTO blogs (name, description, websiteUrl) 
+      VALUES ($1, $2, $3) 
+      RETURNING id;
+    `;
+    const result = await this.pool.query(query, [
+      name,
+      description,
+      websiteUrl,
+    ]);
+    return result.rows[0].id;
+  }
 }
