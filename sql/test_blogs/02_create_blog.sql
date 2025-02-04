6cd55e11605adf98d985485422f472f3aacd5864
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-04 12:37:54 -0400

    Updated create scripts for blof and post

--- sql/test_blogs/02_create_blog.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE blogs (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(15) NOT NULL,
-    description VARCHAR(500) NOT NULL,
-    website_url VARCHAR(100) NOT NULL CHECK (website_url ~ '^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
-);
