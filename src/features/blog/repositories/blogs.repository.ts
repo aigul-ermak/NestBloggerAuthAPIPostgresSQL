9d209a24b7ea7879b7f94716af4fed785f67c718
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-07 19:32:40 -0400

    Updated BlogsRepository: Added method for update blog

--- src/features/blog/repositories/blogs.repository.ts
+++ src/features/blog/repositories/blogs.repository.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Pool } from 'pg';
+import { UpdateBlogDto } from '../dto/update-blog.dto';
 
 @Injectable()
 export class BlogsRepository {
@@ -17,5 +18,28 @@ export class BlogsRepository {
       websiteUrl,
     ]);
     return result.rows[0].id;
+  }
+
+  async update(id: number, updateBlogDto: UpdateBlogDto): Promise<boolean> {
+    const { name, description, websiteUrl } = updateBlogDto;
+
+    const query = `
+      UPDATE blogs 
+        SET 
+        name = COALESCE($1, name), 
+        description = COALESCE($2, description), 
+        website_url = COALESCE($3, website_url)
+    WHERE id = $4
+    RETURNING id;
+    `;
+
+    const result = await this.pool.query(query, [
+      name,
+      description,
+      websiteUrl,
+      id,
+    ]);
+
+    return result.rowCount > 0;
   }
 }
