4632a968da2b556dd0683ac28d14e58e8a4769ab
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-09 18:22:01 -0400

    feat(post to blog): Updated CreatePostDto

--- src/features/post/dto/create-post.dto.ts
+++ src/features/post/dto/create-post.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePostDto {}
+import { IsString, Length } from 'class-validator';
+import { Trim } from '../../../base/decorators/transform/trim';
+import { IsValidBlogId } from '../../../base/decorators/validation/isValidBlogId.decorator';
+
+export class CreatePostDto {
+  @IsString()
+  @Trim()
+  @Length(1, 30, { message: 'Length not correct' })
+  title: string;
+
+  @IsString()
+  @Trim()
+  @Length(1, 100, { message: 'ShortDescription not correct' })
+  shortDescription: string;
+
+  @IsString()
+  @Trim()
+  @Length(1, 1000, { message: 'Content not correct' })
+  content: string;
+
+  @IsString({ message: 'It should be a string' })
+  @IsValidBlogId()
+  blogId: number;
+}
