0469cad048b90aee0d0883f71054abacb62896b1
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-11 15:34:11 -0400

    feat(blog): Added to BlogController endpoint id/posts

--- src/features/blog/blog.controller.ts
+++ src/features/blog/blog.controller.ts
@@ -1,22 +1,37 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req, UseGuards } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { GetBlogByIdUseCaseCommand } from './usecases/getBlogByIdUseCase';
 import { SortBlogsDto } from './dto/sort-blog.input.dto';
 import { GetAllBlogsUseCaseCommand } from './usecases/getAllBlogsUseCase';
-import { Blog } from './entities/blog.entity';
-import { BlogOutputModel } from './dto/blog-output.model';
+import { JwtAuthNullableGuard } from '../../base/guards/auth-guards/jwt-auth-nullable.guard';
+import { SortPostsDto } from '../post/dto/sort-post.input.dto';
+import { GetAllPostsForBlogUseCaseCommand } from './usecases/getAllPostsForBlogUseCase';
 
 @Controller('blogs')
 export class BlogController {
   constructor(private commandBus: CommandBus) {}
 
   @Get()
-  findAll(@Query() sortData: SortBlogsDto) {
+  async findAll(@Query() sortData: SortBlogsDto) {
     return this.commandBus.execute(new GetAllBlogsUseCaseCommand(sortData));
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  async findOne(@Param('id') id: number) {
     return this.commandBus.execute(new GetBlogByIdUseCaseCommand(id));
+  }
+
+  @Get(':id/posts')
+  @UseGuards(JwtAuthNullableGuard)
+  async getPostsForBlog(
+    @Param('id') blogId: number,
+    @Query() sortData: SortPostsDto,
+    @Req() req: Request,
+  ) {
+    const userId = req['userId'];
+
+    return await this.commandBus.execute(
+      new GetAllPostsForBlogUseCaseCommand(blogId, sortData, userId),
+    );
   }
 }
