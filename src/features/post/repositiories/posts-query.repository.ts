8eb78aeb115a5daa600f23f59dcb4e5618498ad2
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-11 15:46:08 -0400

    feat(blog): Added to queries

--- src/features/post/repositiories/posts-query.repository.ts
+++ src/features/post/repositiories/posts-query.repository.ts
@@ -13,4 +13,36 @@ export class PostsQueryRepository {
 
     return result.rows[0];
   }
+
+  async countByBlogId(blogId: number) {
+    const query = `SELECT COUNT(*) FROM posts WHERE blog_id = $1;`;
+    const result = await this.pool.query(query, [blogId]);
+
+    return parseInt(result.rows[0].count, 10);
+  }
+
+  async findPostsByBlogIdPaginated(
+    blogId: number,
+    sort: string,
+    sortDirection: 'asc' | 'desc',
+    page: number,
+    pageSize: number,
+  ) {
+    const validPage = Math.max(page, 1);
+    const offset = (validPage - 1) * pageSize;
+
+    const allowedSortFields = ['created_at', 'title'];
+    const orderBy = allowedSortFields.includes(sort) ? sort : 'created_at';
+
+    const query = `
+    SELECT * FROM posts 
+    WHERE blog_id = $1 
+    ORDER BY ${orderBy} ${sortDirection.toUpperCase()} 
+    LIMIT $2 OFFSET $3;
+  `;
+
+    const result = await this.pool.query(query, [blogId, pageSize, offset]);
+
+    return result.rows;
+  }
 }
