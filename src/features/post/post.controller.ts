ce397f486aab0e2e5f9dd3c5bd39993185e24ee6
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-14 17:30:38 -0400

    test(post): Updated getPostById method in PostController

--- src/features/post/post.controller.ts
+++ src/features/post/post.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PostService } from './post.service';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthNullableGuard } from '../../base/guards/auth-guards/jwt-auth-nullable.guard';
+import { GetPostByIdUseCaseCommand } from './usecases/getPostByIdUseCase';
+import { CommandBus } from '@nestjs/cqrs';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+  constructor(private commandBus: CommandBus) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.postService.findAll();
-  }
+  // @Post()
+  // create(@Body() createPostDto: CreatePostDto) {
+  //   return this.postService.create(createPostDto);
+  // }
+  //
+  // @Get()
+  // findAll() {
+  //   return this.postService.findAll();
+  // }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+  @HttpCode(200)
+  @UseGuards(JwtAuthNullableGuard)
+  async getPostById(@Param('id') id: number, @Req() req: Request) {
+    const userId = req['userId'];
+
+    return await this.commandBus.execute(
+      new GetPostByIdUseCaseCommand(id, userId!),
+    );
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+  //   return this.postService.update(+id, updatePostDto);
+  // }
+  //
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.postService.remove(+id);
+  // }
 }
